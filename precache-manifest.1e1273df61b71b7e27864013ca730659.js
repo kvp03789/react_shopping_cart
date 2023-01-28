201ef3b2bf9c94de72e4f030a045a010eca6b8e5
clay <clay.nichols@gmail.com>
2023-01-27 19:04:18 -0600

    Updates

---
+++ precache-manifest.1e1273df61b71b7e27864013ca730659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b21ddd75b911f21cee51",
+    "url": "kvp03789.github.io/react_shopping_cart/static/css/main.3fc23b9f.chunk.css"
+  },
+  {
+    "revision": "b21ddd75b911f21cee51",
+    "url": "kvp03789.github.io/react_shopping_cart/static/js/main.b21ddd75.chunk.js"
+  },
+  {
+    "revision": "3483ec7423f827aa0b3e",
+    "url": "kvp03789.github.io/react_shopping_cart/static/js/1.3483ec74.chunk.js"
+  },
+  {
+    "revision": "a7a116cdff381ba6671b",
+    "url": "kvp03789.github.io/react_shopping_cart/static/js/runtime~main.a7a116cd.js"
+  },
+  {
+    "revision": "f13ef5d6f0761e08ba1d761b83e952e1",
+    "url": "kvp03789.github.io/react_shopping_cart/static/media/backpack-person.f13ef5d6.webp"
+  },
+  {
+    "revision": "959b5ff1ad11c9e837e6fdbd67587cb3",
+    "url": "kvp03789.github.io/react_shopping_cart/static/media/user-icon.959b5ff1.svg"
+  },
+  {
+    "revision": "91e97b92f0a50ea7946bdf0738623f9d",
+    "url": "kvp03789.github.io/react_shopping_cart/index.html"
+  }
+];
